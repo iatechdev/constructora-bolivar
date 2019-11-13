c474cca99026dd21e97d844f4b345b311c42f64f
mauricio.roldan <mauricio.roldan@iatech.com.co>
2019-11-13 16:17:23 -0500

    ajustes responsive y lista depegable

--- App/js/page-proyectos.js
+++ App/js/page-proyectos.js
@@ -1,3 +1,10 @@
+  //DECLARACION DE VARIABLES GLOBALES
+   var stateDropDownMoreFilters = "OFF";
+   var textBottonDropDownMoreFilters = "Otros filtros";
+  var  stateRotateLongitudBtnMoreFilters = "Metros";
+   //PROCESOS GLOBALES CUANDO SE CARGA EL DOM
+  document.getElementById('textBottonDropDownMoreFiltersHtml').innerHTML = textBottonDropDownMoreFilters;
+
 // alert('hola');
 //domCargado
 (function() {
@@ -39,3 +46,39 @@ function myFunction() {
   var element = document.getElementById("content-frm-busqueda-avanzada-mobile");
   element.classList.toggle("mystyle");
 }
+
+function dropDownMoreFilters(){
+  if (stateDropDownMoreFilters == "OFF"){
+    var aggClassDropDownMoreFilters = document.getElementById("more-filters-interna-proyectos");
+    aggClassDropDownMoreFilters.classList.add("content-mid-filter-search-despegable") ;
+    stateDropDownMoreFilters =  "ON";
+    //------------------------------------------------
+    textBottonDropDownMoreFilters = "Cerrar filtros"
+    document.getElementById('textBottonDropDownMoreFiltersHtml').innerHTML = textBottonDropDownMoreFilters;
+    //------------------------------------------------
+          var removeClassSectionDropDownMoreFilters = document.getElementById("section-drop-down-more-filters");
+      removeClassSectionDropDownMoreFilters.classList.remove("pdd-top-drop-down-more-filters");
+
+    }else if (stateDropDownMoreFilters == "ON") { 
+      var removeClassDropDownMoreFilters = document.getElementById("more-filters-interna-proyectos");
+      removeClassDropDownMoreFilters.classList.remove("content-mid-filter-search-despegable");
+      stateDropDownMoreFilters = "OFF";
+    //------------------------------------------------
+      var textBottonDropDownMoreFilters = "Otros filtros"
+      document.getElementById('textBottonDropDownMoreFiltersHtml').innerHTML = textBottonDropDownMoreFilters;
+    //------------------------------------------------
+          var aggClassSectionDropDownMoreFilters = document.getElementById("section-drop-down-more-filters");
+                aggClassSectionDropDownMoreFilters.classList.add("pdd-top-drop-down-more-filters") ;
+    }
+}
+   function rotateLongitudBtnMoreFilters() {
+    if(stateRotateLongitudBtnMoreFilters == "Metros"){
+        document.getElementById("imageLongitudBtnMoreFilters").src="../assets/images/Group 1657.svg"
+        stateRotateLongitudBtnMoreFilters = "Pies"
+    }else if (stateRotateLongitudBtnMoreFilters == "Pies"){
+           document.getElementById("imageLongitudBtnMoreFilters").src="../assets/images/Group 1471.svg"
+      stateRotateLongitudBtnMoreFilters = "Metros"
+    }
+}
+
+ 
